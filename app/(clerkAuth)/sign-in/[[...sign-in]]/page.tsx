f5e03d1ae1b588b814371426ffc2a770cbe0f4a4
Karan Kumar <karankumarmalhi7755@gmail.com>
2024-09-21 18:09:41 +0500

    add style in sign-in page

--- app/(clerkAuth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(clerkAuth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-  <div className='min-h-screen min-w-[1280px] mx-auto flex items-center justify-center'>
+  <div className='min-h-screen max-w-[1280px] mx-auto flex items-center justify-center'>
     <SignIn />
   </div>
   )
